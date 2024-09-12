e90166f51adbd9888e342728551546835712ba33
kingparks@jeter.eu.org <1570967070@qq.com>
2024-09-12 13:57:59 +0800

    适用于IDEs全系列软件，如：IntelliJ IDEA、AppCode、CLion、DataGrip、GoLand、PhpStorm、PyCharm、Rider、RubyMine、WebStorm，当然也适用Windows/Mac/Linux平台。 此激活方法支持登录账号，支持在线更新，支持跨平台，支持最新版(正版激活)

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 
 var version = 221
 
-var hosts = []string{"http://string.eiyou.fun", "http://string.jeter.eu.org", "http://jetbra.serv00.net:7191", "http://ba.serv00.net:7191"}
+var hosts = []string{"https://idea.jeter.eu.org", "http://129.154.205.7:7191", "http://jetbra.serv00.net:7191", "http://ba.serv00.net:7191"}
 var host = hosts[0]
 var githubPath = "https://mirror.ghproxy.com/https://github.com/kingparks/jetbra-activate/releases/download/latest/"
 var err error
